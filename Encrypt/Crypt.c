d7014eb74318889d084bd23bcad3828ecedb432a
Raphael Preston <raphael.o.preston.20@dartmouth.edu>
2018-04-19 22:38:53 -0300

    while loops are much easier, works when bodySize < cryptSize

--- Encrypt/Crypt.c
+++ Encrypt/Crypt.c
@@ -42,8 +42,7 @@ int main()
 	int matches_size = 30;
 	Matches * matches = newMatches(matches_size);
 	
-	int bodySize = 4;//binary->body->size;
-	int cryptSize = 2;// binary->crypt->size;
+	
 
 	//first for loop
 	printf("Comparing ranges: \n");
@@ -51,58 +50,45 @@ int main()
 	int b_end;
 	int c_start;
 	int c_end;
-	//for (int b = 0; b < bodySize - 2; b++) {
-	//	b_start = bodySize - 2 - b;
-	//	b_end = bodySize - 1;
-	//	c_start = 0;
-	//	c_end = 1 + b;
 
-	//	if (bodySize <= cryptSize) {
-	//		printf("Body:  %i->%i\n", b_start, b_end);
-	//		printf("Crypt: %i->%i\n\n", c_start, c_end);
-	//	}
-	//	else {
-	//		printf("Body:  %i->%i\n", c_start, c_end);
-	//		printf("Crypt: %i->%i\n\n", b_start, b_end);
-	//	}
-	//}
-	//
-	////second for loop
-	//for (int b = 0; b <= cryptSize - bodySize; b++) {
-	//	b_start = 0;
-	//	b_end = bodySize - 1;
-	//	c_start = b;
-	//	c_end = bodySize - 1 + b;
+	int bodySize = 4;//binary->body->size;
+	int cryptSize = 7;// binary->crypt->size;
 
-	//	if (bodySize <= cryptSize) {
-	//		printf("Body:  %i->%i\n", b_start, b_end);
-	//		printf("Crypt: %i->%i\n\n", c_start, c_end);
-	//	}
-	//	else {
-	//		printf("Body:  %i->%i\n", c_start, c_end);
-	//		printf("Crypt: %i->%i\n\n", b_start, b_end);
-	//	}
-	//}
+	b_start = bodySize - 2;
+	b_end = bodySize - 1;
+	c_start = 0;
+	c_end = 1;
 
-	////third for loop
-	//for (int b = 0; b < bodySize - 2; b++) {
-	//	b_start = 0;
-	//	b_end = bodySize - 2 - b;
-	//	c_start = cryptSize - bodySize + 1 + b;
-	//	c_end = cryptSize - 1;
+	//first while loop
+	while (b_start > 0) {
+		printf("Body:  %i->%i\n", b_start, b_end);
+		printf("Crypt: %i->%i\n\n", c_start, c_end);
 
-	//	if (bodySize <= cryptSize) {
-	//		printf("Body:  %i->%i\n", b_start, b_end);
-	//		printf("Crypt: %i->%i\n\n", c_start, c_end);
-	//	}
-	//	else {
-	//		printf("Body:  %i->%i\n", c_start, c_end);
-	//		printf("Crypt: %i->%i\n\n", b_start, b_end);
-	//	}
-	//}
+		b_start--;
+		c_end++;
+	}
+	//second while loop
+	while (c_end < cryptSize - 1) {
+		printf("Body:  %i->%i\n", b_start, b_end);
+		printf("Crypt: %i->%i\n\n", c_start, c_end);
 
-	//would probably work if I made it remove negative values, but trying different approach
-	
+		c_start++;
+		c_end++;
+	}
+	//third while loop
+	printf("After: b_start: %i, b_end: %i, c_start: %i, c_end: %i\n", b_start, b_end, c_start, c_end);
+
+	while (c_start < cryptSize - 1) {
+		printf("Body:  %i->%i\n", b_start, b_end);
+		printf("Crypt: %i->%i\n\n", c_start, c_end);
+
+		b_end--;
+		c_start++;
+	}
+
+
+
+
 	
 	//printMatches(matches);
 
