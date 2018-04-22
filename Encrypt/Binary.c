72b6448f046706a42b68e3bbdc9a7982b7421895
Raphael Preston <raphael.o.preston.20@dartmouth.edu>
2018-04-22 19:31:26 -0300

    Need to revert!! This doesn't work anymore lol

--- Encrypt/Binary.c
+++ Encrypt/Binary.c
@@ -42,19 +42,19 @@ void compareRange(Binary * binary, Matches * matches, int b_start, int c_start, 
 	int m_start = 0;
 
 	for (int i = 0; i < l; i++) {	//run l times
-		//printf("\nAnalyzing b: %i with c: %i\n", b_start + i, c_start + i);
+		printf("\nAnalyzing b: %i with c: %i\n", b_start + i, c_start + i);
 		m = binary->body->arr[b_start + i] + binary->crypt->arr[c_start + i] == 1 ? 2 : 1; //if it's 2 or 0, set to pos.  If it's 1 then its neg.
-		//printf(" Set m to %s, and mode is currently %s\n", m == 0 ? "NULL" : m == 1 ? "POS" : "NEG", mode == 0 ? "NULL" : mode == 1 ? "POS" : "NEG");
+		printf(" Set m to %s, and mode is currently %s\n", m == 0 ? "NULL" : m == 1 ? "POS" : "NEG", mode == 0 ? "NULL" : mode == 1 ? "POS" : "NEG");
 		if (m == mode) {	//continuation
-			//printf(" Continuation detected, duration is now %i.\n", i-m_start+1);
+			printf(" Continuation detected, duration is now %i.\n", i-m_start+1);
 		}
 		else {	//mode is switching, determine if match should be added
 			if (i-m_start >= 2) {	//add match
 				Match * match = newMatch(b_start + m_start, c_start + m_start, i-m_start, mode);
-				//printf(" Adding new %s match: ", mode==0?"NULL":mode==1?"POS":"NEG"); printMatch(match); printf("\n");
+				printf(" Adding new %s match: ", mode==0?"NULL":mode==1?"POS":"NEG"); printMatch(match); printf("\n");
 				addMatch(matches, match);
 			}
-			//else printf(" Match not sufficient length (%i).\n", i-m_start);
+			else printf(" Match not sufficient length (%i).\n", i-m_start);
 			m_start = i;	//starting looking at new match
 			mode = m;
 		}
@@ -62,10 +62,10 @@ void compareRange(Binary * binary, Matches * matches, int b_start, int c_start, 
 	//check last one
 	if (l - m_start >= 2) {	//add match
 		Match * match = newMatch(b_start + m_start, c_start + m_start, l - m_start, m);
-		//printf(" Adding new %s match: ", mode == 0 ? "NULL" : mode == 1 ? "POS" : "NEG"); printMatch(match); printf("\n");
+		printf(" Adding new %s match: ", mode == 0 ? "NULL" : mode == 1 ? "POS" : "NEG"); printMatch(match); printf("\n");
 		addMatch(matches, match);
 	}
-	//else printf(" Match not sufficient length (%i).\n", l - m_start);
+	else printf(" Match not sufficient length (%i).\n", l - m_start);
 }
 
 void bodyCryptAnalysis(Binary * binary, Matches * matches) {
@@ -85,16 +85,16 @@ void bodyCryptAnalysis(Binary * binary, Matches * matches) {
 
 	//first while loop
 	while (b_start > 0) {
-		// printf("Body:  %i->%i\n", b_start, b_end);
-	//	printf("Crypt: %i->%i\n\n", c_start, c_end);
+		printf("Body:  %i->%i\n", b_start, b_end);
+		printf("Crypt: %i->%i\n\n", c_start, c_end);
 		compareRange(binary, matches, b_start, c_start, b_end - b_start + 1);
 		b_start--;
 		c_end++;
 	}
 	//second while loop
 	while (c_end < cryptSize - 1) {
-		// printf("Body:  %i->%i\n", b_start, b_end);
-		//printf("Crypt: %i->%i\n\n", c_start, c_end);
+		printf("Body:  %i->%i\n", b_start, b_end);
+		printf("Crypt: %i->%i\n\n", c_start, c_end);
 		compareRange(binary, matches, b_start, c_start, b_end - b_start + 1);
 		c_start++;
 		c_end++;
@@ -103,8 +103,8 @@ void bodyCryptAnalysis(Binary * binary, Matches * matches) {
 	//printf("After: b_start: %i, b_end: %i, c_start: %i, c_end: %i\n", b_start, b_end, c_start, c_end);
 
 	while (c_start < cryptSize - 1) {
-		// printf("Body:  %i->%i\n", b_start, b_end);
-		// printf("Crypt: %i->%i\n\n", c_start, c_end);
+		printf("Body:  %i->%i\n", b_start, b_end);
+		printf("Crypt: %i->%i\n\n", c_start, c_end);
 		compareRange(binary, matches, b_start, c_start, b_end - b_start + 1);
 		b_end--;
 		c_start++;
